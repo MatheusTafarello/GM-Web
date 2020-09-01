cd45250ea8835078c21ec74408983afe8ee163cf
Giovanni <giovanni.machado@fatec.sp.gov.br>
2020-09-01 20:06:21 -0300

    fix: remove id column
    - add prettier file and all configurate of it

--- src/components/Table/Assisted/AssistedTable.vue
+++ src/components/Table/Assisted/AssistedTable.vue
@@ -8,11 +8,9 @@
     ></FormHeader>
     <div class="table">
       <v-data-table :headers="headers" :items="desserts" class="elevation-1">
-        <template class="actions" v-slot:item.actions="{ item }">
+        <template class="actions" v-slot:[`{item.actions}`]="{ item }">
           <router-link @click="editRow(item)" class="routerLink" to="/">
-            <v-icon small class="mr-2">
-              mdi-pencil
-            </v-icon>
+            <v-icon small class="mr-2">mdi-pencil</v-icon>
           </router-link>
           <v-icon @click.stop="openDialog = true">mdi-delete</v-icon>
           <popupConfig
@@ -27,27 +25,26 @@
   </div>
 </template>
 <script>
-import FormHeader from "@/common-components/FormHeader/FormHeader.vue";
-import popupConfig from "@/common-components/Popup/popupConfig.vue";
+import FormHeader from '@/common-components/FormHeader/FormHeader.vue';
+import popupConfig from '@/common-components/Popup/popupConfig.vue';
 
 export default {
   data() {
     return {
       route: [
-        { name: "Página Inicial", route: "home" },
-        { name: "Gerenciar Assistidas", route: "manage_assisteds" },
+        { name: 'Página Inicial', route: 'home' },
+        { name: 'Gerenciar Assistidas', route: 'manage_assisteds' },
       ],
       headers: [
-        { text: "Nome", align: "start", sortable: true, value: "name" },
-        { text: "CPF", value: "cpf", sortable: false },
-        { text: "Celular", value: "celular", sortable: false },
-        { text: "ID", value: "id", sortable: false },
-        { text: "Ações", value: "actions", sortable: false },
+        { text: 'Nome', align: 'start', sortable: true, value: 'name' },
+        { text: 'CPF', value: 'cpf', sortable: false },
+        { text: 'Celular', value: 'celular', sortable: false },
+        { text: 'Ações', value: 'actions', sortable: false },
       ],
       config: {
         iconWaring: true,
-        textDialog:"Tem certeza?",
-        textFeed: "Deletada com sucesso",
+        textDialog: 'Tem certeza?',
+        textFeed: 'Deletada com sucesso',
       },
       openDialog: false,
     };
@@ -65,16 +62,16 @@ export default {
     initialize() {
       this.desserts = [
         {
-          name: "Robertinha Carla",
-          cpf: "000.000.000-00",
-          celular: "(11) 90000-0000",
-          id: "000001-01.2020",
+          name: 'Robertinha Carla',
+          cpf: '000.000.000-00',
+          celular: '(11) 90000-0000',
+          id: '000001-01.2020',
         },
         {
-          name: "Ana Paula",
-          cpf: "000.000.000-00",
-          celular: "(11) 90000-0000",
-          id: "000002-01.2020",
+          name: 'Ana Paula',
+          cpf: '000.000.000-00',
+          celular: '(11) 90000-0000',
+          id: '000002-01.2020',
         },
       ];
     },
@@ -100,5 +97,4 @@ export default {
   text-decoration: none;
   float: left;
 }
-
 </style>
