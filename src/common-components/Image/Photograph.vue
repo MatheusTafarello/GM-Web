6bd4b5d472e42da95ca88665376d40fdc451edb8
LUCAS A. PAGLIARI <lucas.pagliari@fatec.sp.gov.br>
2020-10-28 00:55:24 +0000

    Merged PR 69: Photograph Component
    Photograph component

---
+++ src/common-components/Image/Photograph.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img
+      v-if="source"
+      :src="require(`@/../../GM-API/src/uploads/${source}`)"
+      :alt="source"
+      :height="size"
+      :width="size"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+    },
+    size: {
+      type: String,
+      default: 'auto',
+    },
+  },
+};
+</script>
